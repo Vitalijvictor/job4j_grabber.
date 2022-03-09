5ad99960617149be450529ec473ef4de72533472
VitalijVictor <vitalijvictor@gmail.com>
2022-03-09 11:01:17 +0100

    1. Quartz [#175122 #272504]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -2,29 +2,41 @@ package ru.job4j.quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
-    public static void main(String[] args) {
+    public void rabbitStart() {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = JobBuilder.newJob(Rabbit.class).build();
             Properties properties = new Properties();
+            try (InputStream in =
+                         Rabbit.class.getClassLoader().getResourceAsStream(
+                                 "rabbit.properties")) {
+                properties.load(in);
+            }
             String interval = properties.getProperty("rabbit.interval");
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(interval));
-                    Trigger trigger = newTrigger()
+                    .withIntervalInSeconds(Integer.parseInt(interval))
+                    .repeatForever();
+            Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+        } catch (SchedulerException | IOException se) {
             se.printStackTrace();
         }
-
+    }
+    public static void main(String[] args) {
+        AlertRabbit alertRabbit = new AlertRabbit();
+        alertRabbit.rabbitStart();
     }
 
     public static class Rabbit implements Job {
